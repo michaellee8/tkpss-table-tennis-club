c6a24f1299ff6bc3911eff4d93a73ec3bb8c3a5d
Michael Lee <ckmichael8@gmail.com>
2017-09-15 21:28:14 +0800

    Implement Comment

--- src/reusable/components/Comment.js
+++ src/reusable/components/Comment.js
@@ -12,13 +12,10 @@ import Card, {
   CardActions
 } from 'material-ui/Card';
 import Collapse from 'material-ui/transitions/Collapse';
-import Avatar from 'material-ui/Avatar';
-import IconButton from 'material-ui/IconButton';
 import Typography from 'material-ui/Typography';
 import red from 'material-ui/colors/red';
 import FavoriteIcon from 'material-ui-icons/Favorite';
 import ShareIcon from 'material-ui-icons/Share';
-import ExpandMoreIcon from 'material-ui-icons/ExpandMore';
 import DeleteIcon from 'material-ui-icons/Delete';
 
 type Props = {
---
+++ src/reusable/components/Comment.stories.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import Comment from './Comment';
+
+function handler(type: string) {
+  switch (type) {
+    case 'delete':
+      action('delete comment');
+      break;
+    case 'navToWriter':
+      action('go to comment writer');
+      break;
+    default:
+  }
+}
+
+storiesOf('Component/Comment/asUser', module)
+  .add('Photo as Avatar', () => (
+    <Comment
+      user={{
+        displayName: 'Michael Test',
+        photoUrl:
+          'https://is1-ssl.mzstatic.com/image/thumb/Purple111/v4/4a/be/60/4abe60e1-6bec-5eb4-ecc5-a4c35771977f/source/512x512bb.jpg'
+      }}
+      isAdmin={false}
+      content="test comment as user with photo as avatar"
+      interactionHandler={handler}
+    />
+  ))
+  .add('Name as Avatar', () => (
+    <Comment
+      user={{
+        displayName: 'Michael Test'
+      }}
+      isAdmin={false}
+      content="test comment as user with name as avatar"
+      interactionHandler={handler}
+    />
+  ));
+
+storiesOf('Component/Comment/asAdmin', module)
+  .add('Photo as Avatar', () => (
+    <Comment
+      user={{
+        displayName: 'Michael Test',
+        photoUrl:
+          'https://is1-ssl.mzstatic.com/image/thumb/Purple111/v4/4a/be/60/4abe60e1-6bec-5eb4-ecc5-a4c35771977f/source/512x512bb.jpg'
+      }}
+      isAdmin={true}
+      content="test comment as admin with photo as avatar"
+      interactionHandler={handler}
+    />
+  ))
+  .add('Name as Avatar', () => (
+    <Comment
+      user={{
+        displayName: 'Michael Test'
+      }}
+      isAdmin={true}
+      content="test comment as admin with name as avatar"
+      interactionHandler={handler}
+    />
+  ));
