9bd072849941d3705a07d61919052e0c8d8983aa
Michael Lee <ckmichael8@gmail.com>
2017-09-15 20:12:52 +0800

    Define User and Comment type

--- src/reusable/types/User.js
+++ src/reusable/types/User.js
@@ -1,7 +1,18 @@
+import type { Comment } from "./Comment";
+
 export type User = {
   id: string,
   searchName: string,
   displayName: string,
   photoUrl: string,
-  privllageLevel: number
+  privllageLevel: number,
+  stats: {
+    hp: number,
+    atk: number,
+    def: number,
+    cot: number,
+    spe: number,
+    int: number
+  },
+  comments: Array<Comment>
 };
