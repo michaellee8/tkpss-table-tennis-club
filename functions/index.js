1289d08728a50c0b216f61f5224bbe4cfbf87f02
michaellee8 <ckmichael8@gmail.com>
2017-10-11 11:27:16 +0800

    Implement new user creation cloud function

--- functions/index.js
+++ functions/index.js
@@ -2,4 +2,28 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 
-exports.createNewUser = functions.auth.user().onCreate(event => {});
+exports.createNewUser = functions.auth.user().onCreate(event => {
+  admin
+    .firestore()
+    .collection("users")
+    .doc(event.data.uid)
+    .set({
+      createTime: Math.floor(Date.now() / 1000),
+      displayName: event.data.displayName,
+      email:event.data.email,
+      joinYear:(new Date()).getFullYear(),
+      lastLoginTime:null,
+      permission:0,
+      photoUrl:event.data.photoUrl,
+      remark:null,
+      score:55,
+      stats:{
+        atk:50,
+        cot:50,
+        def:50,
+        hp:50,
+        int:50,
+        spe:50
+      }
+    });
+});
