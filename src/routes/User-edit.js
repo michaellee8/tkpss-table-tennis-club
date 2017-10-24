97603b44bdc6e9adf96ba7db54fa03158653492a
Michael Lee <ckmichael8@gmail.com>
2017-10-24 23:24:53 +0800

    Implement functional user editor

--- src/routes/User-edit.js
+++ src/routes/User-edit.js
@@ -3,6 +3,7 @@ import Dialog, { DialogTitle, DialogActions } from "material-ui/Dialog";
 import TextField from "material-ui/TextField";
 import Button from "material-ui/Button";
 import firebase from "firebase";
+import List, { ListItem } from "material-ui/List";
 
 export default class extends React.Component {
   props: {
@@ -42,74 +43,81 @@ export default class extends React.Component {
       });
   render() {
     return (
-      <Dialog fullWidth style={{ width: "100%" }} open={this.props.open}>
+      <Dialog style={{ height: "100%" }} fullScreen open={this.props.open}>
         <DialogTitle>{"Editing " + this.props.user.displayName}</DialogTitle>
-        {Object.keys(this.state.editedUserData).map(
-          key =>
-            typeof this.props.user[key] === "object" &&
-            this.props.user !== null ? (
-              Object.keys(this.state.editedUserData[key]).map(innerKey => (
-                <div style={{ width: "100%" }}>
+        <List style={{ maxHeight: "100%", overflow: "auto" }}>
+          {Object.keys(this.state.editedUserData).map(
+            key =>
+              typeof this.props.user[key] === "object" &&
+              this.props.user !== null ? (
+                Object.keys(this.state.editedUserData[key]).map(innerKey => (
+                  <ListItem dense>
+                    {" "}
+                    <TextField
+                      style={{ width: "100%" }}
+                      type={
+                        typeof this.props.user[key][innerKey] === "number" ? (
+                          "number"
+                        ) : (
+                          "string"
+                        )
+                      }
+                      label={key + "." + innerKey}
+                      placeholder={this.props.user[key][innerKey].toString()}
+                      value={this.state.editedUserData[key][innerKey]}
+                      onChange={event => {
+                        event.persist();
+                        console.log(event);
+                        this.setState(prevState => ({
+                          editedUserData: {
+                            ...prevState.editedUserData,
+                            [key]: {
+                              ...prevState.editedUserData[key],
+                              [innerKey]:
+                                typeof this.props.user[key][innerKey] ===
+                                "number"
+                                  ? parseInt(event.target.value)
+                                  : event.target.value
+                            }
+                          }
+                        }));
+                      }}
+                    />
+                  </ListItem>
+                ))
+              ) : (
+                <ListItem dense>
                   {" "}
                   <TextField
                     style={{ width: "100%" }}
                     type={
-                      typeof this.props.user[key][innerKey] === "number"
-                        ? "number"
-                        : "string"
+                      typeof this.props.user[key] === "number" ? (
+                        "number"
+                      ) : (
+                        "string"
+                      )
                     }
-                    label={key + "." + innerKey}
-                    placeholder={this.props.user[key][innerKey].toString()}
-                    value={this.state.editedUserData[key][innerKey]}
+                    label={key}
+                    placeholder={this.props.user[key].toString()}
+                    value={this.state.editedUserData[key]}
                     onChange={event => {
                       event.persist();
                       console.log(event);
                       this.setState(prevState => ({
                         editedUserData: {
                           ...prevState.editedUserData,
-                          [key]: {
-                            ...prevState.editedUserData[key],
-                            [innerKey]:
-                              typeof this.props.user[key][innerKey] === "number"
-                                ? parseInt(event.target.value)
-                                : event.target.value
-                          }
+                          [key]:
+                            typeof this.props.user[key] === "number"
+                              ? parseInt(event.target.value)
+                              : event.target.value
                         }
                       }));
                     }}
                   />
-                </div>
-              ))
-            ) : (
-              <div style={{ width: "100%" }}>
-                {" "}
-                <TextField
-                  style={{ width: "100%" }}
-                  type={
-                    typeof this.props.user[key] === "number"
-                      ? "number"
-                      : "string"
-                  }
-                  label={key}
-                  placeholder={this.props.user[key].toString()}
-                  value={this.state.editedUserData[key]}
-                  onChange={event => {
-                    event.persist();
-                    console.log(event);
-                    this.setState(prevState => ({
-                      editedUserData: {
-                        ...prevState.editedUserData,
-                        [key]:
-                          typeof this.props.user[key] === "number"
-                            ? parseInt(event.target.value)
-                            : event.target.value
-                      }
-                    }));
-                  }}
-                />
-              </div>
-            )
-        )}
+                </ListItem>
+              )
+          )}
+        </List>
         <DialogActions>
           <Button onClick={this.handleCancel} color="primary">
             Cancel
